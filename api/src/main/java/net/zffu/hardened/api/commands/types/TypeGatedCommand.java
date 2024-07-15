257c20b2016e1c1c383b24f444200a1c5137df8a
Zffu <radi0o0@outlook.com>
2024-07-15 23:02:52 +0200

    Renamed ITypeGatedCommand to TypeGatedCommand for consistency

--- api/src/main/java/net/zffu/hardened/api/commands/types/TypeGatedCommand.java
+++ api/src/main/java/net/zffu/hardened/api/commands/types/TypeGatedCommand.java
@@ -6,7 +6,7 @@ import net.zffu.hardened.api.invoker.InvokerType;
  * A {@link net.zffu.hardened.api.commands.Command} that can onlu run with specific {@link net.zffu.hardened.api.invoker.InvokerType}.
  * @since 0.0.1
  */
-public interface ITypeGatedCommand {
+public interface TypeGatedCommand {
 
     /**
      * Gets the list of the {@link InvokerType} that are allowed to run the command.
