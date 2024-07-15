cccb197ce6aa53d4a789adaab2fed8eb38d27f5d
Zffu <radi0o0@outlook.com>
2024-07-15 22:43:29 +0200

    Renamed ICommand to Command

--- api/src/main/java/net/zffu/hardened/api/commands/Command.java
+++ api/src/main/java/net/zffu/hardened/api/commands/Command.java
@@ -1,30 +1,15 @@
 package net.zffu.hardened.api.commands;
 
 /**
- * A Simple command from the Hardened API.
+ * The base of every command created trough the Hardened API.
  * @since 0.0.1
  */
-public class Command {
+public interface Command {
 
     /**
-     * Names of the {@link Command}. The first element is the primary name, the other ones are aliases.
+     * Gets the primary name and aliases of the command.
+     * @return all of the command names of the command.
      */
-    private String[] names;
-
-    /**
-     * Constructs a new {@link Command}.
-     * @param names the names (aliases + the primary name) of the command.
-     */
-    public Command(String... names) {
-        this.names = names;
-    }
-
-    /**
-     * Gets all of the aliases of the command.
-     * @return
-     */
-    public String[] getNames() {
-        return this.names;
-    }
+    String getNames();
 
 }
--- api/src/main/java/net/zffu/hardened/api/commands/executor/ICommandExecutor.java
+++ api/src/main/java/net/zffu/hardened/api/commands/executor/ICommandExecutor.java
@@ -1,11 +1,12 @@
 package net.zffu.hardened.api.commands.executor;
 
+import net.zffu.hardened.api.commands.Command;
 import net.zffu.hardened.api.invoker.ICommandInvoker;
 
 /**
- * An executor of a {@link net.zffu.hardened.api.commands.Command}. Basically does the command logic part.
+ * An executor of a {@link Command}. Basically does the command logic part.
  * @since 0.0.1
- * @see {@link net.zffu.hardened.api.commands.Command}.
+ * @see {@link Command}.
  */
 public interface ICommandExecutor {
 
