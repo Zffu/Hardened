66fe21002237803f950b469f31ebc8a21d7ded62
Zffu <radi0o0@outlook.com>
2024-07-16 00:10:17 +0200

    Added a empty test for now

---
+++ api/src/test/java/net/zffu/hardened/api/tests/CommandBuilderTest.java
@@ -0,0 +1,11 @@
+package net.zffu.hardened.api.tests;
+
+import net.zffu.hardened.api.commands.builder.CommandBuilder;
+
+public class CommandBuilderTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
