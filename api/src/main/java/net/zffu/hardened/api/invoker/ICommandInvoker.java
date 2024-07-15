17a07192741793bb832bc9b56228a3fcaff6918d
Zffu <radi0o0@outlook.com>
2024-07-15 22:34:35 +0200

    Added command invoker interface

---
+++ api/src/main/java/net/zffu/hardened/api/invoker/ICommandInvoker.java
@@ -0,0 +1,11 @@
+package net.zffu.hardened.api.invoker;
+
+/**
+ * The invoker of a {@link net.zffu.hardened.api.commands.Command} call. Allows for multiplatform.
+ * @since 0.0.1
+ */
+public interface ICommandInvoker {
+
+
+
+}
