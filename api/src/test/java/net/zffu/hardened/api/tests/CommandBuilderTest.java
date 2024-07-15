ba909569f8e3789b25f58e933e0b1bed401cfe5a
Zffu <radi0o0@outlook.com>
2024-07-16 00:13:59 +0200

    Added tests for validators

--- api/src/test/java/net/zffu/hardened/api/tests/CommandBuilderTest.java
+++ api/src/test/java/net/zffu/hardened/api/tests/CommandBuilderTest.java
@@ -1,11 +1,25 @@
 package net.zffu.hardened.api.tests;
 
 import net.zffu.hardened.api.commands.builder.CommandBuilder;
+import net.zffu.hardened.api.invoker.CommandInvoker;
+import net.zffu.hardened.api.invoker.InvokerType;
 
 public class CommandBuilderTest {
 
     public static void main(String[] args) {
 
+        // Invokers shouldn't be made like that its just for testing purposes.
+        CommandInvoker sampleInvoker = new CommandInvoker() {
+            @Override
+            public InvokerType getType() {
+                return InvokerType.PLAYER;
+            }
+        };
+
+        CommandBuilder builder = new CommandBuilder("test");
+        builder.allowed(InvokerType.PLAYER); // Only allows player to run the test command
+
+        System.out.println("Validation Result: " + builder.getValidator().validate(builder, sampleInvoker));
     }
 
 }
