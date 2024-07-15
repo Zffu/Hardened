803ac81bca32fa1feb2feabaa41bdd714439c1b1
Zffu <radi0o0@outlook.com>
2024-07-15 23:24:41 +0200

    Added the CommandValidator interface

---
+++ api/src/main/java/net/zffu/hardened/api/commands/validator/CommandValidator.java
@@ -0,0 +1,20 @@
+package net.zffu.hardened.api.commands.validator;
+
+import net.zffu.hardened.api.commands.Command;
+import net.zffu.hardened.api.invoker.CommandInvoker;
+
+/**
+ * <p>Validates that a {@link Command} can be ran by the provided {@link CommandInvoker}.</p>
+ * @since 0.0.1
+ */
+public interface CommandValidator<T extends Command > {
+
+    /**
+     * Validates the {@link CommandInvoker} for a {@link Command}
+     * @param command the {@link Command} to validate for.
+     * @param invoker the invoker.
+     * @return true if the invoker is allowed to run the command or else false
+     */
+    boolean validate(T command, CommandInvoker invoker);
+
+}
