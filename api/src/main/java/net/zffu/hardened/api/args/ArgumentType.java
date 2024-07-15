a80366b85edddb0ada504ca007a4066ec313590e
Zffu <radi0o0@outlook.com>
2024-07-16 01:28:41 +0200

    Added argument type enum

---
+++ api/src/main/java/net/zffu/hardened/api/args/ArgumentType.java
@@ -0,0 +1,12 @@
+package net.zffu.hardened.api.args;
+
+/**
+ * A type value of an argument in a {@link net.zffu.hardened.api.commands.Command}.
+ * @since 0.0.1
+ */
+public enum ArgumentType {
+
+    STRING,
+    INT
+
+}
