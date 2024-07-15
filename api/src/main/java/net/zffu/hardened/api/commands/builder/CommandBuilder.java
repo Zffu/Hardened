29790527472d9b8392ad76713b776ce78ef24216
Zffu <radi0o0@outlook.com>
2024-07-15 22:57:16 +0200

    Started implementing the Command interface funcs into CommandBuilder

--- api/src/main/java/net/zffu/hardened/api/commands/builder/CommandBuilder.java
+++ api/src/main/java/net/zffu/hardened/api/commands/builder/CommandBuilder.java
@@ -3,28 +3,23 @@ package net.zffu.hardened.api.commands.builder;
 import net.zffu.hardened.api.commands.Command;
 
 /**
- * <p>A builder for {@link Command}.</p>
- * <p>This is recommended if you want to make command fast but do not care about any minor memory usage that could result.</p>
+ * <p>Variant of the {@link net.zffu.hardened.api.commands.Command} interface.</p>
+ * <p>This variant isn't the default one as it implements every single feature that commands can have in the Hardened for the builder.</p>
+ * <p>This means that this can be used only if you do not mind the extra memory usage that will come from the empty variable fields from the unused interfaces.</p>
  * @since 0.0.1
  * @see {@link Command}
  */
-public class CommandBuilder {
+public class CommandBuilder implements Command {
 
+    private String[] names;
 
-
-    /**
-     * <p>Variant of the {@link net.zffu.hardened.api.commands.Command} interface.</p>
-     * <p>This variant isn't the default one as it implements every single feature that commands can have in the Hardened for the builder.</p>
-     * <p>This means that this can be used only if you do not mind the extra memory usage that will come from the empty variable fields from the unused interfaces.</p>
-     * @since 0.0.1
-     */
-    private class BuiltCommand implements Command {
-
-        @Override
-        public String getNames() {
-            return null;
-        }
+    public CommandBuilder(String[] names) {
+        this.names = names;
     }
 
+    @Override
+    public String[] getNames() {
+        return this.names;
+    }
 
 }
