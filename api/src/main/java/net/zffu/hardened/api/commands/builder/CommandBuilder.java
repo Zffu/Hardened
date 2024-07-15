9a7021473fd2982dea937ea5ef9603b0cef77629
Zffu <radi0o0@outlook.com>
2024-07-15 22:54:31 +0200

    Added the BuiltCommand class into the CommandBuilder

--- api/src/main/java/net/zffu/hardened/api/commands/builder/CommandBuilder.java
+++ api/src/main/java/net/zffu/hardened/api/commands/builder/CommandBuilder.java
@@ -11,4 +11,20 @@ import net.zffu.hardened.api.commands.Command;
 public class CommandBuilder {
 
 
+
+    /**
+     * <p>Variant of the {@link net.zffu.hardened.api.commands.Command} interface.</p>
+     * <p>This variant isn't the default one as it implements every single feature that commands can have in the Hardened for the builder.</p>
+     * <p>This means that this can be used only if you do not mind the extra memory usage that will come from the empty variable fields from the unused interfaces.</p>
+     * @since 0.0.1
+     */
+    private class BuiltCommand implements Command {
+
+        @Override
+        public String getNames() {
+            return null;
+        }
+    }
+
+
 }
