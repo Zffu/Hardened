a0546312365a6c915c28a118580434791a3c3737
Zffu <radi0o0@outlook.com>
2024-07-15 22:51:01 +0200

    Started working on command building

---
+++ api/src/main/java/net/zffu/hardened/api/commands/builder/CommandBuilder.java
@@ -0,0 +1,14 @@
+package net.zffu.hardened.api.commands.builder;
+
+import net.zffu.hardened.api.commands.Command;
+
+/**
+ * <p>A builder for {@link Command}.</p>
+ * <p>This is recommended if you want to make command fast but do not care about any minor memory usage that could result.</p>
+ * @since 0.0.1
+ * @see {@link Command}
+ */
+public class CommandBuilder {
+
+
+}
