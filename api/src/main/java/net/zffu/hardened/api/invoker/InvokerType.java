754e3da786c02d847b4ae21922143f3c2486d8bd
Zffu <radi0o0@outlook.com>
2024-07-15 22:44:45 +0200

    Added InvokerType enum

---
+++ api/src/main/java/net/zffu/hardened/api/invoker/InvokerType.java
@@ -0,0 +1,13 @@
+package net.zffu.hardened.api.invoker;
+
+/**
+ * Possible types of {@link ICommandInvoker}.
+ * @since 0.0.1
+ */
+public enum InvokerType {
+
+    PLAYER,
+    CONSOLE,
+    OTHER
+
+}
