5fdc50249197d0f7462aae61145236e170db6404
Zffu <radi0o0@outlook.com>
2024-07-16 21:33:04 +0200

    Updated builder command example

--- api-examples/src/main/java/net/zffu/hardened/examples/api/BuilderCommandExample.java
+++ api-examples/src/main/java/net/zffu/hardened/examples/api/BuilderCommandExample.java
@@ -4,6 +4,7 @@ import net.zffu.hardened.api.args.Argument;
 import net.zffu.hardened.api.args.ArgumentTypes;
 import net.zffu.hardened.api.commands.builder.CommandBuilder;
 import net.zffu.hardened.api.context.CommandContext;
+import net.zffu.hardened.api.invoker.CommandInvoker;
 import net.zffu.hardened.api.invoker.InvokerType;
 
 /**
@@ -14,16 +15,9 @@ public class BuilderCommandExample {
     public static void main(String[] args) {
 
         // Creates the base of the command builder
-        CommandBuilder builder = new CommandBuilder() {
-
-            // Everything in that function will be ran after validation.
-            @Override
-            public void execute(CommandContext commandContext) {
-                String name = commandContext.get(0, String.class); // Gets the first argument of the command.
-                System.out.println("Hello " + name);
-            }
-
-        }
+        CommandBuilder commandBuilder = new CommandBuilder("test").runAction((ctx) -> {
+            System.out.println("Hello " + ctx.getInvoker().getType().name());
+        })
         // Adds properties to the CommandBuilder.
         .allowed(InvokerType.PLAYER) // Makes sure only players can run this command
         .args(new Argument(ArgumentTypes.STRING.get())); // Adds a string argument
