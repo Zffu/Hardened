373d17d0f793fd0dcefbc16405eea4064e94b1dc
Zffu <radi0o0@outlook.com>
2024-07-16 20:58:36 +0200

    Added argument formatting test

---
+++ api/src/test/java/net/zffu/hardened/api/tests/ArgumentFormattingTest.java
@@ -0,0 +1,22 @@
+package net.zffu.hardened.api.tests;
+
+import net.zffu.hardened.api.args.Argument;
+import net.zffu.hardened.api.args.ArgumentTypes;
+import net.zffu.hardened.api.commands.builder.CommandBuilder;
+import net.zffu.hardened.api.context.CommandContext;
+
+public class ArgumentFormattingTest {
+
+    public static void main(String[] args) {
+        CommandBuilder command = new CommandBuilder() {
+            @Override
+            public void execute(CommandContext commandContext) {
+
+            }
+        };
+        command.args(new Argument(ArgumentTypes.STRING.get()));
+
+        CommandContext ctx = CommandContext.preFormatArguments(null, command, "test", new String[]{"test"});
+    }
+
+}
