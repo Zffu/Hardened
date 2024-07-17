4c4ab8c2d068d38182c4995e656c8b2975ea4c31
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-17 21:09:52 +0200

    Corrected CommandBuilder javadoc's since version

--- api/src/main/java/net/zffu/hardened/api/commands/builder/CommandBuilder.java
+++ api/src/main/java/net/zffu/hardened/api/commands/builder/CommandBuilder.java
@@ -13,7 +13,7 @@ import java.util.function.Supplier;
  * <p>Variant of the {@link net.zffu.hardened.api.commands.Command} interface.</p>
  * <p>This variant isn't the default one as it implements every single feature that commands can have in the Hardened for the builder.</p>
  * <p>This means that this can be used only if you do not mind the extra memory usage that will come from the empty variable fields from the unused interfaces.</p>
- * @since 0.0.1
+ * @since 1.0.0
  * @see {@link Command}
  */
 public class CommandBuilder extends BuilderCommand {
