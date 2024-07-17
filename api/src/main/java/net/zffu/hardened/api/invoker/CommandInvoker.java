08a633f2993f507e00f2daf47bc120c9e3150360
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-17 21:07:46 +0200

    Corrected CommandInvoker javadoc's since version

--- api/src/main/java/net/zffu/hardened/api/invoker/CommandInvoker.java
+++ api/src/main/java/net/zffu/hardened/api/invoker/CommandInvoker.java
@@ -2,7 +2,7 @@ package net.zffu.hardened.api.invoker;
 
 /**
  * The invoker of a {@link net.zffu.hardened.api.commands.Command} call. Allows for multiplatform.
- * @since 0.0.1
+ * @since 1.0.0
  */
 public interface CommandInvoker {
 
