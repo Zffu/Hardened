28265a2a1e01d5c7d0c7683ad5819f8b135736c8
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-17 21:58:05 +0200

    Added fromCommandSender function

--- spigot/src/main/java/net/zffu/hardened/spigot/handler/SpigotCommandHandler.java
+++ spigot/src/main/java/net/zffu/hardened/spigot/handler/SpigotCommandHandler.java
@@ -1,9 +1,14 @@
 package net.zffu.hardened.spigot.handler;
 
 import net.zffu.hardened.api.context.CommandContext;
+import net.zffu.hardened.api.invoker.CommandInvoker;
+import net.zffu.hardened.spigot.invokers.OtherInvoker;
+import net.zffu.hardened.spigot.invokers.PlayerInvoker;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
 /**
  * <p>A handler for {@link net.zffu.hardened.api.commands.Command}.</p>
@@ -20,4 +25,15 @@ public class SpigotCommandHandler implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
     }
+
+    /**
+     * Turns a {@link CommandSender} into a {@link CommandInvoker}.
+     * @param sender the {@link CommandSender}
+     * @return the {@link CommandInvoker}
+     */
+    public static CommandInvoker fromCommandSender(CommandSender sender) {
+        if(sender instanceof Player) return new PlayerInvoker((Player) sender);
+        return new OtherInvoker(sender);
+    }
+
 }
