4dc3807e85d25e1ea79598e616016ffda5cd5904
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-17 21:09:05 +0200

    Corrected PermissionCommand javadoc's since version

--- api/src/main/java/net/zffu/hardened/api/commands/types/PermissionCommand.java
+++ api/src/main/java/net/zffu/hardened/api/commands/types/PermissionCommand.java
@@ -2,7 +2,7 @@ package net.zffu.hardened.api.commands.types;
 
 /**
  * A {@link net.zffu.hardened.api.commands.Command} that requires the {@link net.zffu.hardened.api.invoker.CommandInvoker} to have a specific permission.
- * @since 0.0.1
+ * @since 1.0.0
  */
 public interface PermissionCommand {
 
