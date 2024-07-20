6fb03d1853da125bc966473819556237ef88f5aa
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-20 19:45:26 +0200

    Added annotations in Command class

--- api/src/main/java/net/zffu/hardened/api/commands/Command.java
+++ api/src/main/java/net/zffu/hardened/api/commands/Command.java
@@ -3,6 +3,8 @@ package net.zffu.hardened.api.commands;
 import net.zffu.hardened.api.commands.validator.CommandValidator;
 import net.zffu.hardened.api.context.CommandContext;
 import net.zffu.hardened.api.invoker.CommandInvoker;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * The base of every command created trough the Hardened API.
@@ -14,32 +16,32 @@ public interface Command<T extends CommandValidator> {
      * Gets the primary name of the command.
      * @return
      */
-    String getPrimaryName();
+    @NotNull String getPrimaryName();
 
     /**
      * Gets the aliases of the command.
      * @return
      */
-    String[] getAliases();
+    @NotNull String[] getAliases();
 
     /**
      * <p>Gets the {@link CommandValidator} for that {@link Command}.</p>
      * <p>The {@link CommandValidator} varies depending on the implementation of the {@link Command} class used.</p>
      * @return the {@link CommandValidator}.
      */
-    T getValidator();
+    @Nullable T getValidator();
 
     /**
      * Performs the {@link Command} logic with the provided {@link CommandInvoker}.
      * @param commandContext the {@link CommandInvoker}
      */
-    void execute(CommandContext commandContext);
+    void execute(@NotNull CommandContext commandContext);
 
     /**
      * Runs the {@link Command} and checks using the {@link CommandValidator}.
      * @param commandContext
      */
-    default void run(CommandContext commandContext) {
+    default void run(@NotNull CommandContext commandContext) {
         if(getValidator() == null || getValidator().validate(this, commandContext)) execute(commandContext);
     }
 
