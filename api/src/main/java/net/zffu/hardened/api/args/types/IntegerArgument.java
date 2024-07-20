d727cf178b0465f137c3f21b5d98fc90845c35db
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-20 20:54:31 +0200

    Made better javadoc for IntegerArgument

--- api/src/main/java/net/zffu/hardened/api/args/types/IntegerArgument.java
+++ api/src/main/java/net/zffu/hardened/api/args/types/IntegerArgument.java
@@ -3,7 +3,7 @@ package net.zffu.hardened.api.args.types;
 import net.zffu.hardened.api.args.ArgumentType;
 
 /**
- * An Integer {@link ArgumentType}.
+ * <p>A int {@link ArgumentType}.</p>
  * @since 1.0.0
  */
 public class IntegerArgument implements ArgumentType<Integer> {
