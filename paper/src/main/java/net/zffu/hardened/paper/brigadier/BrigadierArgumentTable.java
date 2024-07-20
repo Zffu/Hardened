80dfd0308e5a178d4acc59e17d8f3c0b1d6012a1
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-20 21:43:43 +0200

    Added argument conversion in brigadier table

--- paper/src/main/java/net/zffu/hardened/paper/brigadier/BrigadierArgumentTable.java
+++ paper/src/main/java/net/zffu/hardened/paper/brigadier/BrigadierArgumentTable.java
@@ -3,21 +3,25 @@ package net.zffu.hardened.paper.brigadier;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import net.zffu.hardened.api.args.ArgumentTypes;
+import net.zffu.hardened.paper.utils.DoubleFunction;
 
 /**
  * <p>Allows to convert Hardened Argument types into Brigadier argument types.</p>
  */
 public enum BrigadierArgumentTable {
 
-    STRING(StringArgumentType.string()),
-    INT(IntegerArgumentType.integer());
+    STRING(StringArgumentType.string(), (ctx, id) -> StringArgumentType.getString(ctx, id)),
+    INT(IntegerArgumentType.integer(), (ctx, id) -> IntegerArgumentType.getInteger(ctx, id));
 
 
     private ArgumentType type;
+    private DoubleFunction<Object, CommandContext, String> function;
 
-    private BrigadierArgumentTable(ArgumentType type) {
+    private BrigadierArgumentTable(ArgumentType type, DoubleFunction<Object, CommandContext, String> func) {
         this.type = type;
+        this.function = func;
     }
 
     public ArgumentType get() {
@@ -26,6 +30,10 @@ public enum BrigadierArgumentTable {
 
     public static ArgumentType from(ArgumentTypes types) {
         return BrigadierArgumentTable.values()[types.ordinal()].get();
+    }
+
+    public Object getFromCtx(CommandContext ctx, String id) {
+        return this.function.apply(ctx, id);
     }
 
 }
