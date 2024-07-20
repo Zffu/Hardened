6fff231258a0cceb6fb9050ba5b34ec41ae796c5
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-20 20:44:04 +0200

    Made better javadoc for PermissionCommand

--- api/src/main/java/net/zffu/hardened/api/commands/types/PermissionCommand.java
+++ api/src/main/java/net/zffu/hardened/api/commands/types/PermissionCommand.java
@@ -1,15 +1,18 @@
 package net.zffu.hardened.api.commands.types;
 
+import net.zffu.hardened.api.invoker.CommandInvoker;
+import net.zffu.hardened.api.commands.validator.CommandValidator;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * A {@link net.zffu.hardened.api.commands.Command} that requires the {@link net.zffu.hardened.api.invoker.CommandInvoker} to have a specific permission.
+ * <p>A command that only allows invoker with a specific permission to execute its logic.</p>
+ * <p><b>Warn: </b>No checks for this are there by default, those checks should be implemented in the {@link CommandValidator}</p>
  * @since 1.0.0
  */
 public interface PermissionCommand {
 
     /**
-     * Gets the required permission to be allowed to run this command.
+     * <p>Gets the permission that the {@link CommandInvoker} should have to be able to run the command.</p>
      * @return
      */
     @NotNull String getRequiredPermission();
