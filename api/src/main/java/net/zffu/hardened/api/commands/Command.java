a45813fb633fedde28b1a5935654869e2a443291
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-20 20:32:16 +0200

    Made cleaner Command javadoc

--- api/src/main/java/net/zffu/hardened/api/commands/Command.java
+++ api/src/main/java/net/zffu/hardened/api/commands/Command.java
@@ -7,39 +7,42 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * The base of every command created trough the Hardened API.
+ * <p>The base of every command registered trough the Hardened API. Any command that uses this API should implement this.</p>
+ * <p>An interface that stores basic command metadata, the {@link CommandValidator} and the execution function.</p>
+ * @param <T> the class of the {@link CommandValidator} for that command.
  * @since 1.0.0
  */
 public interface Command<T extends CommandValidator> {
 
     /**
-     * Gets the primary name of the command.
-     * @return
+     * <p>Gets the primary name of the command.</p>
+     * <p>The primary name is the true name (not alias) of the command.</p>
+     * @return the primary name of the command.
      */
     @NotNull String getPrimaryName();
 
     /**
-     * Gets the aliases of the command.
-     * @return
+     * <p>Gets the aliases of the command.</p>
+     * @return the aliases of the command as a {@link String[]}
      */
     @NotNull String[] getAliases();
 
     /**
-     * <p>Gets the {@link CommandValidator} for that {@link Command}.</p>
-     * <p>The {@link CommandValidator} varies depending on the implementation of the {@link Command} class used.</p>
-     * @return the {@link CommandValidator}.
+     * <p>Gets the command validator. The command validator is used to validate that a specific context can run the command.</p>
+     * @return the {@link CommandValidator}
      */
     @Nullable T getValidator();
 
     /**
-     * Performs the {@link Command} logic with the provided {@link CommandInvoker}.
-     * @param commandContext the {@link CommandInvoker}
+     * <p>Triggers the command logic based on the provided context without doing any validation beforehand.</p>
+     * <p><b>Note: </b>The run function should be used as it has validator checks.</p>
+     * @param commandContext the {@link CommandContext} provided to the logic.
      */
     void execute(@NotNull CommandContext commandContext);
 
     /**
-     * Runs the {@link Command} and checks using the {@link CommandValidator}.
-     * @param commandContext
+     * <p>Performs validator checks and executes the command logic using the provided context if the validator allows the context to run the command.</p>
+     * @param commandContext the {@link CommandContext} provided.
      */
     default void run(@NotNull CommandContext commandContext) {
         if(getValidator() == null || getValidator().validate(this, commandContext)) execute(commandContext);
