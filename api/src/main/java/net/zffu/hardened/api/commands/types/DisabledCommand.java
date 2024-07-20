9726cd405e5c3b0f0850a1c848723dea113bb8d4
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-07-20 20:45:13 +0200

    Made better javadoc for DisabledCommand

--- api/src/main/java/net/zffu/hardened/api/commands/types/DisabledCommand.java
+++ api/src/main/java/net/zffu/hardened/api/commands/types/DisabledCommand.java
@@ -1,13 +1,16 @@
 package net.zffu.hardened.api.commands.types;
 
+import net.zffu.hardened.api.commands.validator.CommandValidator;
+
 /**
- * A {@link net.zffu.hardened.api.commands.Command} that can be disabled or not.
+ * <p>A command that can be simply toggled to be disabled or not. While being disabled, the command logic will never run.</p>
+ * <p><b>Warn: </b>No checks for this are there by default, those checks should be implemented in the {@link CommandValidator}</p>
  * @since 1.0.0
  */
 public interface DisabledCommand {
 
     /**
-     * Determines if the command is currently disabled.
+     * <p>Determines if the command is disabled currently.</p>
      * @return
      */
     boolean isDisabled();
