64d81ecbc699afdea7c95abb4010c6ba0032d784
Zffu <radi0o0@outlook.com>
2024-07-25 22:15:48 +0200

    Seperated the execute func from Command

---
+++ api/src/main/java/net/zffu/hardened/api/commands/CommandExecution.java
@@ -0,0 +1,20 @@
+package net.zffu.hardened.api.commands;
+
+import net.zffu.hardened.api.context.CommandContext;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * <p>Contains the execute function of a {@link Command}.</p>
+ * @since 1.0.0
+ * @see {@link Command}
+ */
+public interface CommandExecution {
+
+    /**
+     * <p>Triggers the command logic based on the provided context without doing any validation beforehand.</p>
+     * <p><b>Note: </b>The run function should be used as it has validator checks.</p>
+     * @param commandContext the {@link CommandContext} provided to the logic.
+     */
+    void execute(@NotNull CommandContext commandContext);
+
+}
