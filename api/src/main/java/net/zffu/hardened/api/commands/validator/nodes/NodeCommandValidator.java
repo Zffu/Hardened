bec8e11b05f5cb635eb9bddf59a4e1e94ac8463e
Zffu <radi0o0@outlook.com>
2024-07-25 21:48:27 +0200

    Turned nodes into a List to facilitate builder impl

--- api/src/main/java/net/zffu/hardened/api/commands/validator/nodes/NodeCommandValidator.java
+++ api/src/main/java/net/zffu/hardened/api/commands/validator/nodes/NodeCommandValidator.java
@@ -5,16 +5,24 @@ import net.zffu.hardened.api.commands.validator.CommandValidator;
 import net.zffu.hardened.api.context.CommandContext;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A {@link CommandValidator} that uses the node command validation approach.
  * @since 1.0.0
  */
 public class NodeCommandValidator<T extends Command> implements CommandValidator<T> {
 
-    private ValidatorNode<T>[] nodes;
+    private List<ValidatorNode<T>> nodes;
 
     public NodeCommandValidator(ValidatorNode<T>... nodes) {
-        this.nodes = nodes;
+        this.nodes = Arrays.asList(nodes);
+    }
+
+    public NodeCommandValidator() {
+        this.nodes = new ArrayList<>();
     }
 
     @Override
