bf3e37e99e8f052be117c54dbe7123c8424b4be8
Zffu <radi0o0@outlook.com>
2024-07-26 00:29:26 +0200

    Added argument in BuilderCommandExample

--- api-examples/src/main/java/net/zffu/hardened/examples/api/BuilderCommandExample.java
+++ api-examples/src/main/java/net/zffu/hardened/examples/api/BuilderCommandExample.java
@@ -1,5 +1,8 @@
 package net.zffu.hardened.examples.api;
 
+import com.sun.org.apache.xpath.internal.Arg;
+import net.zffu.hardened.api.args.Argument;
+import net.zffu.hardened.api.args.Arguments;
 import net.zffu.hardened.api.commands.Command;
 import net.zffu.hardened.api.commands.builder.CommandBuilder;
 import net.zffu.hardened.api.invoker.InvokerType;
@@ -13,7 +16,8 @@ public class BuilderCommandExample {
         Command command = new CommandBuilder("test") // Creates a command with the primary name test
                 .type(InvokerType.PLAYER) // Only allows players to run the command
                 .permission("test.permission") // Makes the player need the permission "test.permission" in order to run the command.
-                .execute((ctx) -> System.out.println("Hello " + ctx.getInvoker().getType())) // Sets the command actions
+                .argument(Arguments.string().min(3).max(16))
+                .execute((ctx) -> System.out.println("Hello " + ctx.get(0,String.class))) // Sets the command actions
                 .build(); // Builds out the command
     }
 
