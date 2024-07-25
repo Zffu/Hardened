03f990e229e96616fc68ae6203187300e6bcee36
Zffu <radi0o0@outlook.com>
2024-07-26 00:31:10 +0200

    Added string canParse function

--- api/src/main/java/net/zffu/hardened/api/args/types/StringArgument.java
+++ api/src/main/java/net/zffu/hardened/api/args/types/StringArgument.java
@@ -1,6 +1,7 @@
 package net.zffu.hardened.api.args.types;
 
 import net.zffu.hardened.api.args.ArgumentType;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * <p>A String {@link ArgumentType}.</p>
@@ -11,5 +12,10 @@ public class StringArgument implements ArgumentType<String> {
     @Override
     public String fromString(String s) {
         return s;
+    }
+
+    @Override
+    public boolean canParse(@NotNull String s) {
+        return true;
     }
 }
