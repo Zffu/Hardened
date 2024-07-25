bf5f60f15fd9303ee56db44ca541e50ea8c49256
Zffu <radi0o0@outlook.com>
2024-07-25 22:02:22 +0200

    Added aliases builder function

--- api/src/main/java/net/zffu/hardened/api/commands/builder/CommandBuilder.java
+++ api/src/main/java/net/zffu/hardened/api/commands/builder/CommandBuilder.java
@@ -6,4 +6,19 @@ package net.zffu.hardened.api.commands.builder;
  * @see {@link net.zffu.hardened.api.commands.Command}
  */
 public class CommandBuilder extends BuilderCommand {
+
+    public CommandBuilder(String primaryName) {
+        this.primaryName = primaryName;
+    }
+
+    /**
+     * Sets the aliases of the command.
+     * @param aliases
+     * @return
+     */
+    public CommandBuilder aliases(String... aliases) {
+        this.aliases = aliases;
+        return this;
+    }
+
 }
