32031d38f8fd8e7994788856c25da72c85c9350a
Zffu <radi0o0@outlook.com>
2024-07-25 21:45:55 +0200

    Started remaking builder

--- api/src/main/java/net/zffu/hardened/api/commands/builder/BuilderCommand.java
+++ api/src/main/java/net/zffu/hardened/api/commands/builder/BuilderCommand.java
@@ -1,71 +1,36 @@
 package net.zffu.hardened.api.commands.builder;
 
-import net.zffu.hardened.api.args.Argument;
 import net.zffu.hardened.api.commands.Command;
-import net.zffu.hardened.api.commands.types.ArgCommand;
-import net.zffu.hardened.api.commands.types.DisabledCommand;
-import net.zffu.hardened.api.commands.types.PermissionCommand;
-import net.zffu.hardened.api.commands.types.TypeGatedCommand;
-import net.zffu.hardened.api.invoker.InvokerType;
+import net.zffu.hardened.api.commands.validator.CommandValidator;
+import net.zffu.hardened.api.commands.validator.nodes.NodeCommandValidator;
+import net.zffu.hardened.api.context.CommandContext;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
+public class BuilderCommand implements Command {
 
-/**
- * <p>Variant of the {@link net.zffu.hardened.api.commands.Command} interface.</p>
- * <p>This variant isn't the default one as it implements every single feature that commands can have in the Hardened for the builder.</p>
- * <p>This means that this can be used only if you do not mind the extra memory usage that will come from the empty variable fields from the unused interfaces.</p>
- * @since 1.0.0
- * @see {@link Command}
- */
-public abstract class BuilderCommand implements Command<BuilderCommandValidator>, TypeGatedCommand, ArgCommand, PermissionCommand, DisabledCommand {
-
-    private static BuilderCommandValidator validator = new BuilderCommandValidator();
-
-    protected String primaryName;
-
-    protected List<InvokerType> allowedTypes;
-
-    protected String[] aliases;
-
-    protected Argument[] arguments;
-
-    protected String permission;
-
-    protected boolean disabled;
+    private String primaryName;
+    private String[] aliases;
+    private NodeCommandValidator<BuilderCommand> validator;
 
     @Override
-    public String getPrimaryName() {
-        return primaryName;
+    public @NotNull String getPrimaryName() {
+        return this.primaryName;
     }
 
     @Override
-    public String[] getAliases() {
-        return aliases;
+    public @NotNull String[] getAliases() {
+        return this.aliases;
+    }
+
+    @Nullable
+    @Override
+    public CommandValidator getValidator() {
+        return this.validator;
     }
 
     @Override
-    public BuilderCommandValidator getValidator() {
-        return validator;
-    }
+    public void execute(@NotNull CommandContext commandContext) {
 
-    @Override
-    public List<InvokerType> getAllowedInvokers() {
-        return this.allowedTypes;
-    }
-
-    @Override
-    public Argument[] getArguments() {
-        return this.arguments;
-    }
-
-    @Override
-    public String getRequiredPermission() {
-        return this.permission;
-    }
-
-    @Override
-    public boolean isDisabled() {
-        return this.disabled;
     }
 }
