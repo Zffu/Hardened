b8f34611c550ac10c40af70ef3f34537568cbc30
Zffu <radi0o0@outlook.com>
2024-07-26 00:33:30 +0200

    Added float argument

---
+++ api/src/main/java/net/zffu/hardened/api/args/types/FloatArgument.java
@@ -0,0 +1,27 @@
+package net.zffu.hardened.api.args.types;
+
+import net.zffu.hardened.api.args.ArgumentType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * <p>A double {@link ArgumentType}.</p>
+ * @since 1.0.0
+ */
+public class FloatArgument implements ArgumentType<Float> {
+
+    @Override
+    public @Nullable Float fromString(@NotNull String s) {
+        return Float.valueOf(s);
+    }
+
+    @Override
+    public boolean canParse(@NotNull String s) {
+        try {
+            Float.valueOf(s);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
