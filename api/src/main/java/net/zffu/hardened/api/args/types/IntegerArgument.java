25e31bc02d6de8ec6bea11d64053f43796c8f6dd
Zffu <radi0o0@outlook.com>
2024-07-26 00:30:54 +0200

    Added int canParse function

--- api/src/main/java/net/zffu/hardened/api/args/types/IntegerArgument.java
+++ api/src/main/java/net/zffu/hardened/api/args/types/IntegerArgument.java
@@ -1,6 +1,7 @@
 package net.zffu.hardened.api.args.types;
 
 import net.zffu.hardened.api.args.ArgumentType;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * <p>A int {@link ArgumentType}.</p>
@@ -11,5 +12,15 @@ public class IntegerArgument implements ArgumentType<Integer> {
     @Override
     public Integer fromString(String s) {
         return Integer.valueOf(s);
+    }
+
+    @Override
+    public boolean canParse(@NotNull String s) {
+        try {
+            Integer.valueOf(s);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
