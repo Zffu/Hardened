17451d646c7564df7d02b610db91f2ab6651224f
Zffu <103074097+Zffu@users.noreply.github.com>
2025-12-06 09:18:54 +0100

    feat: made Bungee Command handler handle use the invoker cache

--- bungee/src/main/java/net/zffu/hardened/bungeecord/handler/BungeecordCommandHandler.java
+++ bungee/src/main/java/net/zffu/hardened/bungeecord/handler/BungeecordCommandHandler.java
@@ -1,8 +1,11 @@
 package net.zffu.hardened.bungeecord.handler;
 
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.zffu.hardened.api.context.CommandContext;
+import net.zffu.hardened.api.invoker.CommandInvoker;
+import net.zffu.hardened.api.registrar.CommandRegistrar;
 import net.zffu.hardened.bungeecord.BungeeInvoker;
 
 /**
@@ -11,19 +14,23 @@ import net.zffu.hardened.bungeecord.BungeeInvoker;
  */
 public class BungeecordCommandHandler extends Command {
 
+    private final CommandRegistrar registrar;
     private final net.zffu.hardened.api.commands.Command command;
     private boolean parseArgs;
 
-    public BungeecordCommandHandler(net.zffu.hardened.api.commands.Command command) {
+    public BungeecordCommandHandler(net.zffu.hardened.api.commands.Command command, CommandRegistrar registrar) {
         super(command.getPrimaryName(), "", command.getAliases());
         this.command = command;
+        this.registrar = registrar;
         this.parseArgs = !this.command.getArguments().getArguments().isEmpty();
     }
 
     @Override
     public void execute(CommandSender commandSender, String[] args) {
+        CommandInvoker invoker = commandSender instanceof ProxiedPlayer ? this.registrar.instance.getPlayerInvoker(((ProxiedPlayer) commandSender).getUniqueId()) : this.registrar.instance.consoleInvoker;
+
         CommandContext ctx = new CommandContext(
-                new BungeeInvoker(commandSender),
+                invoker,
                 (this.parseArgs ? CommandContext.preFormatArguments(this.command, args) : null),
                 this.getName()
         );
