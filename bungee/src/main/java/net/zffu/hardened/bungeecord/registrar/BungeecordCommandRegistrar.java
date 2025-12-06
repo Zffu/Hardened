654ef949d69482fabf357440a00bccb16b2f8e26
Zffu <103074097+Zffu@users.noreply.github.com>
2025-12-06 08:26:16 +0100

    added bungee impl base

---
+++ bungee/src/main/java/net/zffu/hardened/bungeecord/registrar/BungeecordCommandRegistrar.java
@@ -0,0 +1,14 @@
+package net.zffu.hardened.bungeecord.registrar;
+
+import net.zffu.hardened.api.commands.Command;
+import net.zffu.hardened.api.registrar.CommandRegistrar;
+import org.jetbrains.annotations.NotNull;
+
+public class BungeecordCommandRegistrar implements CommandRegistrar {
+
+    @Override
+    public void register(@NotNull Command<?> command) {
+
+    }
+
+}
