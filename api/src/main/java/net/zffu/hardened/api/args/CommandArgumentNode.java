893fbe0fa2fc68cb60e89d7734e15ad122ff2c11
Zffu <103074097+Zffu@users.noreply.github.com>
2025-12-06 10:10:43 +0100

    feat: made arguments traversable

--- api/src/main/java/net/zffu/hardened/api/args/CommandArgumentNode.java
+++ api/src/main/java/net/zffu/hardened/api/args/CommandArgumentNode.java
@@ -1,16 +1,28 @@
 package net.zffu.hardened.api.args;
 
+import net.zffu.hardened.api.command.CommandTreeTraversable;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Represents an argument inside a command tree.
  * <b>This argument doesn't have choices and is just a basic argument.</b>
  * @param <K> the datatype
  */
-public class CommandArgumentNode<K> {
+public class CommandArgumentNode<K> implements CommandTreeTraversable {
 
     public final String name;
+
+    public @NotNull CommandTreeTraversable nextNode;
 
     public CommandArgumentNode(String name) {
         this.name = name;
     }
 
+    @Override
+    public boolean traverse() {
+        // TODO: Add argument parsing to this
+
+        if (this.nextNode != null) return this.nextNode.traverse();
+        return true;
+    }
 }
