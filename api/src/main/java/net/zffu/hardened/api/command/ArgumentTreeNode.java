4276e3277343b2e68ca2ed34df971cd7d99724b8
Zffu <103074097+Zffu@users.noreply.github.com>
2025-12-06 10:04:39 +0100

    feat: started working on a tree type structure

---
+++ api/src/main/java/net/zffu/hardened/api/command/ArgumentTreeNode.java
@@ -0,0 +1,17 @@
+package net.zffu.hardened.api.command;
+
+/**
+ * <p>An argument node inside a command tree.</p>
+ * @since 1.0.0
+ */
+public interface ArgumentTreeNode {
+
+    /**
+     * Traverses the argument tree node
+     *
+     * @return true if command was okay, false if not
+     */
+    //TODO: add command ctx
+    boolean traverse();
+
+}
