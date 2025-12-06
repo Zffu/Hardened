651d54d3c15f88c88671a60627095714d7a57f6f
Zffu <103074097+Zffu@users.noreply.github.com>
2025-12-06 10:38:40 +0100

    feat: add string type

---
+++ api/src/main/java/net/zffu/hardened/api/args/types/StringType.java
@@ -0,0 +1,29 @@
+package net.zffu.hardened.api.args.types;
+
+import net.zffu.hardened.api.args.ArgumentType;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class StringType implements ArgumentType<String> {
+
+    private int maxLength;
+
+    public StringType() {
+        this.maxLength = -1;
+    }
+
+    @Override
+    public String parse(String argumentStr) {
+        if(this.maxLength > 0 && argumentStr.length() > this.maxLength) return null;
+
+        return argumentStr;
+    }
+
+    @Override
+    public @Nullable Collection<String> getChoices() {
+        return null;
+    }
+
+}
