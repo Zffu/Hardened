c0427b2b83b99319cacfc621c673c47ef119544d
Zffu <103074097+Zffu@users.noreply.github.com>
2025-12-06 08:41:02 +0100

    feat: finished command handler

--- bungee/src/main/java/net/zffu/hardened/bungeecord/handler/BungeecordCommandHandler.java
+++ bungee/src/main/java/net/zffu/hardened/bungeecord/handler/BungeecordCommandHandler.java
@@ -3,7 +3,12 @@ package net.zffu.hardened.bungeecord.handler;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
 import net.zffu.hardened.api.context.CommandContext;
+import net.zffu.hardened.bungeecord.BungeeInvoker;
 
+/**
+ * <p>A bungeecord handler for {@link net.zffu.hardened.api.commands.Command}.</p>
+ * @since 1.0.0
+ */
 public class BungeecordCommandHandler extends Command {
 
     private final net.zffu.hardened.api.commands.Command command;
@@ -16,7 +21,13 @@ public class BungeecordCommandHandler extends Command {
     }
 
     @Override
-    public void execute(CommandSender commandSender, String[] strings) {
+    public void execute(CommandSender commandSender, String[] args) {
+        CommandContext ctx = new CommandContext(
+                new BungeeInvoker(commandSender),
+                (this.parseArgs ? CommandContext.preFormatArguments(this.command, args) : null),
+                this.getName()
+        );
 
+        this.command.execute(ctx);
     }
 }
